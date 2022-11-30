4500374a6fa6a95a3d376e81dbf54342e46b9003
Alp Senay <alps2789@nycstudents.net>
2022-11-30 09:25:41 -0500

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,52 @@
-import java.util.Scanner;
+import java.util.Scanner; //imports scanner
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in); //initializes scanner
 
+        // stores user input as variable name
         System.out.println("What is your name?: ");
         String name = scan.nextLine();
 
+        // calls the greeting for user (welcome message)
         Physics obj = new Physics();
         System.out.println(obj.Greeting(name));
 
+        // stores user input as variable age
         System.out.println("How old are you?");
         int age = scan.nextInt();
         scan.nextLine(); //clears buffer
 
+        // calls the greeting for user (secret code)
         Physics obj1 = new Physics();
         System.out.println(obj1.Greeting(age));
 
+        //stores confirmation as next double entered by user
         double confirm = scan.nextDouble();
         scan.nextLine(); //clears buffer
 
+        // runs if user chooses to proceed with program
         if (confirm == (age + 24)) {
-            System.out.println("What would you like to learn about? \n Please choose an option: \n -History \n -Regents Phys Topics \n -Equations");
+            System.out.println("What would you like to learn about? \n Please choose an option (case sensitive!): \n -History \n -Regents Phys Topics \n -Equations");
             String topicChoice = scan.nextLine();
 
             Physics obj2 = new Physics();
             obj2.choiceOfTopics(topicChoice);
 
+        // prompts uer to try again; ends program
         } else {
             while (confirm != (age + 24)) {
-                System.out.println("That's not the correct code! Please try again if you want to run the bot. Otherwise, please have a good day. Hope to see you soon!");
+                System.out.println("That's not the correct code! Please try again if you want to run the bot. \n Otherwise, please have a good day. Hope to see you soon!");
                 confirm = scan.nextInt();
             }
         }
+
+        //calls toString
+        Physics obj3 = new Physics();
+        System.out.println(obj3.toString(name));
+
+        //calls and prints method to find length of user's name
+        Physics obj4 = new Physics();
+        System.out.println(obj4.lengthOfName(name));
     }
 }
--- src/Physics.java
+++ src/Physics.java
@@ -1,37 +1,62 @@
-import java.util.Scanner;
+/**
+ * The Physics class represents physics. Physics represents the user's name, age,
+ * topics and subtopics of physics, the topic chosen by the user, and the secret
+ * code the user has to enter
+ */
 public class Physics {
-    private String name;
-    private String topicChoice;
-    private int age;
-    private int secretCode;
-    private int topics;
-    private int subTopics;
-    private int total;
+    private String name; //stores user's name
+    private String topicChoice; //stores topic chosen by user
+    private int age; //stores user's age
+    private double secretCode; //stores secret code
+    private int topics; //stores num of topics in course
+    private int subTopics; //stores num of subtopics in course
+    private int total; //stores total num of topics + subtopics
 
+    /** Constructor for Physics class. This initializes each variable above. Strings are
+     * set to empty Strings, doubles are set to 0.0, ints are set to 0
+     */
+// initializes variables
     public Physics() {
         name = "";
         topicChoice = "";
         age = 0;
-        secretCode = 0;
+        secretCode = 0.0;
         topics = 0;
         subTopics = 0;
         total = 0;
     }
 
-
+    /** Greeting method for the Physics class. This method will return a String welcoming
+     * the user based on the name inputted when the program asks for their name
+     * @param name represents the name of the user
+     * @return returns a String in a properly formatted sentence containing the welcome message
+     */
+// greets user by name; bot gives a bit of info about itself
     public String Greeting(String name) {
             String str = "";
             str += "Hi " + name + ". Welcome to the physBot helper! \nI am here to help you with your questions about physics.";
             return str;
     }
 
+    /** Greeting method for Physics class. This method will return a String containing
+     * the secret code, which is the user's age + 24
+     * @param age represents the age of the user
+     * @return returns a String in a properly formatted sentence containing the secret code
+     */
+//asks user for their age and converts it into a secret code
     public String Greeting(int age) {
         String str = "";
-        secretCode = Math.abs(age + 24);
+        secretCode = (double) (Math.abs(age + 24));
         str += "Your secret code is " + secretCode + " . Please enter that to continue.";
         return str;
     }
 
+    /** choiceOfTopics method for Physics class. This method will return normally but
+     * does not provide a result value to its caller. The if then statements will run
+     * based on the topic chosen by the user.
+     * @param topicChoice represents the topic chosen by the user
+     */
+// if then statement that prints specific information based on user choice
     public void choiceOfTopics(String topicChoice) {
         if ((topicChoice.equals("History") || (topicChoice.equals("history")))) {
             System.out.println("Physics is a branch of science whose primary topics of study are matter and energy.\n It is divided into classical and modern physics.\n Developed in the 20th century,\n modern physics include quantum mechanics,special relativity, and general relativity.\n Classical physics deals with theories of physics that may not be precise since it is old.");
@@ -39,10 +64,7 @@ public class Physics {
             int topics = 5;
             int subTopics = 19;
             int total = topics + subTopics;
-            //public String toString() {
-            //    System.out.println("There are a total of" + topics + "topics and " + subTopics + " subtopics in this course. These include: ");
-            //}
-
+            System.out.println("There are a total of" + topics + "topics and " + subTopics + " subtopics in this course. These include: ");
             System.out.println();
             System.out.println("Mechanics \n -Kinematics \n -Statics \n -Dynamics \n -Momentum");
             System.out.println();
@@ -59,5 +81,25 @@ public class Physics {
             System.out.println("vf = vi + at \n final velocity is equal to initial velocity added to acceleration multiplied by time");
             System.out.println("p = mv \n power is equal to mass times velocity");
         }
+    }
+
+    /** toString method for the Physics class. This method will return a String
+     * that shows a thank you message for the user using the program
+     * @param name represents the name of the user
+     * @return returns a properly formatted String containing a thank you
+     */
+// thanks user with their name
+    public String toString(String name) {
+        return " \nThank you " + name + " for using this program!";
+    }
+
+    /** lengthOfName method for the Physics class. This method will return the
+     * length of the name inputted by the user
+     * @param name represents the name of the user
+     * @return returns the length of the name
+     */
+// returns length of the user's name inputted
+    public int lengthOfName(String name){
+        return name.length();
     }
 }
